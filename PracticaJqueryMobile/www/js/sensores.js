75dd838fbf272bd8028858a070a8d530166107f4
alu0100819182 <alu0100819182@ull.edu.es>
2016-11-24 16:55:39 +0000

    Sensores parte 1 hechos

--- PracticaJqueryMobile/www/js/sensores.js
+++ PracticaJqueryMobile/www/js/sensores.js
@@ -1,45 +1,67 @@
-function brujula(){
+var opciones = {frequency:1000};
+
+function brujula() {
   navigator.compass.getCurrentHeading(onSuccess, onError);
 }
 
-var opciones = {frequency:3000};
-
-function brujulaCont(){
+function brujulaCont() {
   navigator.compass.watchHeading(onSucces, onError, opciones);
 }
 
-function onSuccess(heading){
-  alert("Heading" + heading.magneticHeading);
+function onSuccess(heading) {
+  var elemento = document.getElementById("uno");
+  elemento.innerHTML = 'Grados con respecto al norte: ' + heading.magneticHeading;
 };
 
-function onError(error){
-  alert('ERROR' + error.code);
+function onSucces(heading) {
+  var elemento = document.getElementById("dos");
+  elemento.innerHTML = 'Grados con respecto al norte: ' + heading.magneticHeading;
+};
+
+function onError(error) {
+  alert('ERROR ' + error.code);
 }
 
-function accelerometro(){
+function acelerometro(){
   navigator.accelerometer. getCurrentAcceleration(onAccept, onMistake);
 }
 
-function accelerometroCont(){
-  navigator.accelerometer.watchAcceleration(onAccept, onMistake, opciones);
+function acelerometroCont(){
+  navigator.accelerometer.watchAcceleration(onAccep, onMistake, opciones);
+}
+
+function onAccept(acceleration) {
+  var elemento = document.getElementById("tres");
+  elemento.innerHTML = 'Accceleration X:' + acceleration.x + '\n' +
+                       'Acceleration Y: ' + acceleration.y + '\n' +
+                       'Acceleration Z: ' + acceleration.z + '\n' +
+                       'Timestamp: ' + acceleration.timestamp + '\n';
+}
+
+function onAccep(acceleration) {
+  var elemento = document.getElementById("cuatro");
+  elemento.innerHTML = 'Accceleration X:' + acceleration.x + '\n' +
+                       'Acceleration Y: ' + acceleration.y + '\n' +
+                       'Acceleration Z: ' + acceleration.z + '\n' +
+                       'Timestamp: ' + acceleration.timestamp + '\n';
 }
 
 function onMistake(){
 	alert('onError!');
 }
 
-function onAccept(acceleration){
-alert('Accceleration X:' + acceleration.x + '\n' +
-'Acceleration Y: ' + acceleration.y + '\n' + 
-'Acceleration Z: ' + acceleration.z + '\n' +
-'Timestamp: ' + acceleration.timestamp + '\n');
+/*var latInfo = 28.4836297;
+var lonInfo = -16.3226178;*/
 
+function GPS() {
+	navigator.geolocation.getCurrentPosition(siRespuesta, enError);
 }
 
-function GPS(){
-	navigator.geolocation.getCurrentPosition(siRespuesta, onError);
+function siRespuesta(position) {
+  var elemento = document.getElementById("quinto");
+  elemento.innerHTML = 'Latitud: ' + position.coords.latitude + '\n' + 'Longitud: ' + position.coords.longitude;
 }
-function siRepuesta(position){
-	alert('Latitud: ' + position.coords.latitude +'/n'
-	'Longitud: ' + position.coords.longitude);
+
+function enError(error) {
+  alert('ERROR ' + error.code);
 }
