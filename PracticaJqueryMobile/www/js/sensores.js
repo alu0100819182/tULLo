b1d80a2ba76ee947173ed7ec0569cc99467e4f36
alu0100819182 <alu0100819182@ull.edu.es>
2016-12-10 19:43:34 +0000

    GPS Funcionando

--- PracticaJqueryMobile/www/js/sensores.js
+++ PracticaJqueryMobile/www/js/sensores.js
@@ -53,8 +53,25 @@ function onMistake(){
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*var latInfo = 28.4836297;
-var lonInfo = -16.3226178;*/
+var latInfoini = 28.483647;
+var lonInfoini = -16.322654;
+var latInfofin = 28.482525;
+var lonInfofin = -16.321164;
+
+var latfymini = 28.482457;
+var lonfymini = -16.321143;
+var latfymfin = 28.481570;
+var lonfymfin = -16.320200;
+
+var latbioini = 28.480932;
+var lonbioini = -16.320637;
+var latbiofin = 28.479647;
+var lonbiofin = -16.319512;
+
+var latpruebaini = 28.483647;
+var lonpruebaini = -16.323367;
+var latpruebafin = 28.483163;
+var lonpruebafin = -16.322967;
 
 function GPS() {
 	navigator.geolocation.getCurrentPosition(siRespuesta, enError);
@@ -65,8 +82,17 @@ function GPSCont() {
 }
 
 function siRespuesta(position) {
-  var elemento = document.getElementById("quinto");
-  elemento.innerHTML = 'Latitud: ' + position.coords.latitude + '\n' + 'Longitud: ' + position.coords.longitude;
+  var elemento = document.getElementById("localizacion");
+  if((latpruebaini >= position.coords.latitude) && (position.coords.latitude >= latpruebafin)
+    && (lonpruebaini <= position.coords.longitude) && (position.coords.longitude <= lonpruebafin)) {
+      elemento.innerHTML = 'Estas en casa' + "<br>";
+      var boton = document.createElement("button");
+      boton.innerHTML = 'Casa de Omar';
+      boton.onclick = function MyFuncion () {
+        alert("Hola");
+      }
+      elemento.appendChild(boton);
+  }
 }
 
 function siRespuestaa(position) {
@@ -75,5 +101,6 @@ function siRespuestaa(position) {
 }
 
 function enError(error) {
-  alert('ERROR ' + error.code);
+  var elemento = document.getElementById("localizacion");
+  elemento.innerHTML = 'Fallo';
 }
